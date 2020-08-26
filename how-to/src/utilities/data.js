6076a5adac8ce3c53b9d048ad1f5505324aac5ee
Bikesh <Biku@Bikeshs-MacBook-Air-2.local>
2020-08-26 14:12:29 -0700

    fixed minour bugs

--- how-to/src/utilities/data.js
+++ how-to/src/utilities/data.js
@@ -1,22 +1,24 @@
 const FAKE_DATA = [
   {
     id: 0,
-    name: "React Redux",
-    description: "Learn React Redux in 30 days",
+    name: "How to Build React App",
+    description:
+      "A guide on creating your first ever react app as a developer!",
     category: "Javascript",
   },
   {
     id: 1,
-    name: "Node.js",
+    name: "What is Node.js and How to implent in your project?",
     description:
-      "Learn a Backend Language in about 2 months without any computer prior knowledge ",
+      "A simple guide on node.js to overcome difficulties you have when creating an app. ",
     category: "Programming",
   },
   {
     id: 2,
-    name: "Angular",
-    description: "Learn Angular at your own pace online",
-    category: "Angular",
+    name: "How To Change your Computer battery",
+    description:
+      "You don't need a computer technician to change your battery you can do it by yourself by following these steps.",
+    category: "Computer",
   },
 ];
 
